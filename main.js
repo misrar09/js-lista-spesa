bac5bebf307f44525ba7d07f3ecbcb12f81aa613
misrar09 <misrar09@gmail.com>
2023-11-03 14:59:42 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const shoppingList = [ "Milk", "Eggs", "Rice", "Pasta", "Meat", "Water", "Coffee", "Pesto", "Fruits", "Onions"]
+
+
+let items = 0
+while(items < shoppingList.length){
+    console.log(shoppingList[items]);
+    items++;
+}
+
